2e0a9d62f28fdb5e0d3027c20636f6615c865708
Bianca Cristina <phon-bia@hotmail.com>
2018-08-26 13:41:46 -0300

    Consulta simples com query methods

--- src/main/java/br/ufu/facom/workshopmongo/repository/PostRepository.java
+++ src/main/java/br/ufu/facom/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package br.ufu.facom.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import br.ufu.facom.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	// Extends "MongoRepository" pra indicar que estamos usando o BD MongoDB
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);  // Retorna posts que contenham esse title ignorando o fato de ser maiscula ou minuscula
 }
