d5de8acd011205f90e95d0a5e277b63e0ce153db
Bianca Cristina <phon-bia@hotmail.com>
2018-08-26 11:23:12 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/br/ufu/facom/workshopmongo/config/Instantiation.java
@@ -0,0 +1,31 @@
+package br.ufu.facom.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import br.ufu.facom.workshopmongo.domain.User;
+import br.ufu.facom.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		// Esse metodo esta servindo para iniciar o banco de dados com alguns dados pre-definidos no metodo
+		
+		userRepository.deleteAll(); // Limpa a colecao que ja estiver presente no MongoDB
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+	
+}
