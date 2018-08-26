be586733904a517d9a5319f4839aa4f0eb8018da
Bianca Cristina <phon-bia@hotmail.com>
2018-08-26 13:14:25 -0300

    Referenciando os posts do usuario

--- src/main/java/br/ufu/facom/workshopmongo/domain/User.java
+++ src/main/java/br/ufu/facom/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package br.ufu.facom.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 // collection="user"eh opcional, caso nao coloque o Spring Data vai mapear a colecao com o nome da classe so que iniciando com letra minuscula!
@@ -15,6 +18,10 @@ public class User implements Serializable {
 	
 	private String name;
 	private String email;
+	
+	@DBRef(lazy=true) // Essa anotacoa indica que no "conjunto" do User, os posts serao uma colecao de referencias
+	// O lazy faz com que os posts so sejam carregados caso eu explicitamente acessa-los, do contrario quando eu acessar o usuario vira apenas os dados basicos do user
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {	
 	}
@@ -50,6 +57,14 @@ public class User implements Serializable {
 		this.email = email;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
